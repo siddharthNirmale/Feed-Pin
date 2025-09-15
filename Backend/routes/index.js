2106afab473fa42b0965a19b597b9a66df94df12
siddharthNirmale <siddharth175nirmale1@gmail.com>
2025-09-16 00:07:18 +0530

    Add file upload functionality with multer and update profile view
    - Implemented multer for handling file uploads in multer.js
    - Created a form in profile.ejs for image uploads with a hidden input
    - Added an upload icon that triggers the file input click event
    - Automatically submits the form upon file selection
    - Updated profile image display logic to reflect uploaded images

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,12 +1,73 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('./users');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+passport.use(new localStrategy(userModel.authenticate()));
+const upload = require('./multer');
 
 
-router.get('/', function(req, res, next) {
-  res.render('index' );
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
-router.get('/register', function(req, res, next) {
-  res.render('register' );
+
+router.get('/register', function (req, res, next) {
+  res.render('register');
 });
+
+router.get('/profile', isLoggedIn,  async function (req, res, next) {
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  })
+  res.render('profile', { user });
+});
+router.post('/fileUpload', isLoggedIn, upload.single('image'), async function (req, res, next) {
+  const user = await userModel.findOne({
+    username: req.session.passport.user
+  })
+  user.profileImage =  req.file.filename;
+  await user.save();
+  res.redirect('/profile');
+});
+
+router.post('/register', function (req, res, next) {
+  const data = new userModel({
+    username: req.body.username,
+    email: req.body.email,
+    contact: req.body.contact
+
+  });
+  userModel.register(data, req.body.password)
+    .then(function () {
+      passport.authenticate('local')(req, res, function () {
+        res.redirect('/profile');
+      });
+    })
+});
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/profile',
+  failureRedirect: '/'
+}), function (req, res, next) {
+}
+);
+
+router.get('/logout', function (req, res, next) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    res.redirect('/');
+  }
+};
+
+
+
+
 
 module.exports = router;
